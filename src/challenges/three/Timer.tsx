3bcb70fbb265986574590d305c17a0e96a688204
Don Gyver Gabito <gyver.26@gmail.com>
2021-10-12 00:23:51 +0800

    Completed challenge three

--- src/challenges/three/Timer.tsx
+++ src/challenges/three/Timer.tsx
@@ -1,15 +1,80 @@
 import { Button, HStack, Text } from "@chakra-ui/react";
-import React from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+function useCountdownTimer() {
+  const [running, setRunning] = useState(false);
+  const [time, setTime] = useState(0);
+
+  const start = () => {
+    setRunning(true);
+  };
+
+  const pause = () => {
+    setRunning(false);
+  };
+
+  const reset = () => {
+    setRunning(false);
+    setTime(0);
+  };
+
+  useEffect(() => {
+    if (running) {
+      const a = setInterval(() => setTime((prevTime) => prevTime + 100), 100);
+
+      return () => clearInterval(a);
+    }
+  }, [running]);
+
+  return { time, running, start, pause, reset };
+}
+
+type Countdown = {
+  time: number;
+  running: boolean;
+  start: () => void;
+  pause: () => void;
+  reset: () => void;
+};
+
+const initialContextValue = {
+  time: 0,
+  running: false,
+  start: () => null,
+  pause: () => null,
+  reset: () => null
+};
+
+const CountdownProvider = createContext<Countdown>(initialContextValue);
 
 type TimerProps = {
   children: JSX.Element;
 };
 
 export default function Timer({ children }: TimerProps) {
-  return children;
+  const countdown = useCountdownTimer();
+  return (
+    <CountdownProvider.Provider value={countdown}>
+      {children}
+    </CountdownProvider.Provider>
+  );
 }
 
 export function TimerDisplay() {
+  const { time } = useContext(CountdownProvider);
+  const [timerText, setTimerText] = useState("0:00:0");
+
+  useEffect(() => {
+    const minutes = Math.floor(time / (1000 * 60))
+      .toString()
+      .padStart(2, "0");
+    const seconds = Math.floor((time % (1000 * 60)) / 1000)
+      .toString()
+      .padStart(2, "0");
+    const milliSeconds = Math.floor((time % 1000) / 100).toString();
+    setTimerText(`${minutes}:${seconds}:${milliSeconds}`);
+  }, [time]);
+
   return (
     <Text
       fontSize="7xl"
@@ -18,17 +83,25 @@ export function TimerDisplay() {
         fontVariantNumeric: "tabular-nums"
       }}
     >
-      0:00:0
+      {timerText}
     </Text>
   );
 }
 
 export function TimerControls() {
+  const { time, start, pause, reset, running } = useContext(CountdownProvider);
   return (
     <HStack>
-      <Button colorScheme="green">Start</Button>
-      <Button colorScheme="red">Pause</Button>
-      <Button>Reset</Button>
+      {running ? (
+        <Button onClick={pause} colorScheme="red">
+          Pause
+        </Button>
+      ) : (
+        <Button onClick={start} colorScheme="green">
+          Start
+        </Button>
+      )}
+      {(time !== 0 || running) && <Button onClick={reset}>Reset</Button>}
     </HStack>
   );
 }
